ecf053bc7097a23edd9855779563aa57ec016220
MaksymKarpenko <karpenko.maxim87gmail.com>
2018-08-18 18:43:33 +0300

    add meetup list and meetup card

---
+++ src/components/Meetup/Meetup.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card>
+          <v-card-title>
+            <h6 class="primary--text">My Meetup</h6>
+          </v-card-title>
+          <v-card-media
+            src="https://ob9a8415roh4djoj110c31a1-wpengine.netdna-ssl.com/wp-content/uploads/2013/02/barcelona-aerial-view.jpg"
+            height="400px"
+          ></v-card-media>
+          <v-card-text>
+            <div class="info--text"> 17th July 2017 - Where it takes place </div>
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa numquam et nesciunt incidunt inventore perferendis atque, corrupti, provident autem cum velit. Expedita, similique error perspiciatis sed deserunt commodi dolorem velit.
+            </div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn class="primary">Register</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
