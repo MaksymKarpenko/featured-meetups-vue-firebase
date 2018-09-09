cab9584819bfd2016879e1d028346472780e508e
MaksymKarpenko <karpenko.maxim87gmail.com>
2018-09-09 23:29:46 +0300

    changed sign up page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import * as firebase from 'firebase'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import { store } from './store'
@@ -17,5 +18,14 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBdH8uQUdh7F5GNEQf6UVSlDZSwCrK6WUY',
+      authDomain: 'featured-meetup.firebaseapp.com',
+      databaseURL: 'https://featured-meetup.firebaseio.com',
+      projectId: 'featured-meetup',
+      storageBucket: 'featured-meetup.appspot.com'
+    })
+  }
 })
